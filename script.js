357cc0f618a14352583ebfceab8d70746af90e25
LisaBee224 <lisakbuch@gmail.com>
2016-06-22 12:56:42 -0400

    initial work

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("button").click(function(e){
+    $(e.target).parent("body").addClass('active');
+    $("#modal").show();
+    $("#close").click(function(e){
+      $(e.target).parent().hide();
+      $("body").removeClass();
+    });
+  });
+
+});

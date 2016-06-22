3fb7973204fcf57568beb92f04211e200f2dd58d
LisaBee224 <lisakbuch@gmail.com>
2016-06-22 14:24:05 -0400

    Add fade in

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("button").click(function(e){
     $(e.target).parent("body").addClass('active');
-    $("#modal").show();
+    $("#modal").slideDown("slow");
     $(".close").click(function(e){
       $(e.target).parent().hide();
       $("body").removeClass();

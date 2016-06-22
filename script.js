02dd0e00f74b4b37c68c3cefcbeeb79c502a919c
LisaBee224 <lisakbuch@gmail.com>
2016-06-22 13:02:45 -0400

    start styling

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("button").click(function(e){
     $(e.target).parent("body").addClass('active');
     $("#modal").show();
-    $("#close").click(function(e){
+    $(".close").click(function(e){
       $(e.target).parent().hide();
       $("body").removeClass();
     });
